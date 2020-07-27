cf6c21dd9f611304927a064d0ae07134c41bf172
WangZhong <619324237@qq.com>
2020-07-27 14:00:05 +0800

    build: xx

--- packages/griffith/src/index.js
+++ packages/griffith/src/index.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: wangzhong
+ * @Date: 2020-07-24 18:09:18
+ * @LastEditors: wangzhong
+ * @LastEditTime: 2020-07-27 13:59:51
+ * @FilePath: /griffith/packages/griffith/src/index.js
+ */
+
 export {default} from './components/PlayerContainer'
 export {ExternalContext as MessageContext} from './contexts/Message'
 export {VideoSourceContext} from './contexts/VideoSource'
 export {default as Controller} from './components/Controller'
 export {default as Layer} from './components/Layer'
+// add log
